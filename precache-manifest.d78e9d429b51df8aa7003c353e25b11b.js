7c37029161e424bf38785fe8524a80c37bfc68ae
j.bennett <61255945+jben817@users.noreply.github.com>
2020-08-12 19:01:31 -0400

    Updates

---
+++ precache-manifest.d78e9d429b51df8aa7003c353e25b11b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8788b99ea1927547c54335f2642037d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14de953fbdbdf7b48f96",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3f0b3fa894f0b80ceeee",
+    "url": "/static/css/main.ce7b92c3.chunk.css"
+  },
+  {
+    "revision": "14de953fbdbdf7b48f96",
+    "url": "/static/js/2.a335d44c.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.a335d44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0b3fa894f0b80ceeee",
+    "url": "/static/js/main.a2001a6a.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  },
+  {
+    "revision": "3b7fcc48452d24cb700a3d3df416dec5",
+    "url": "/static/media/JB resume.3b7fcc48.pdf"
+  },
+  {
+    "revision": "6efcb5bc1d6e387dd81498fee41fe5e4",
+    "url": "/static/media/NYME project photo.6efcb5bc.png"
+  },
+  {
+    "revision": "561623d92f0e11dd0da47b9c435c98c6",
+    "url": "/static/media/day planner.561623d9.png"
+  },
+  {
+    "revision": "ee84f874959fd13602c5ff49013c604d",
+    "url": "/static/media/generator website.ee84f874.png"
+  },
+  {
+    "revision": "4190a326e28f4143d626d7bb760c8a14",
+    "url": "/static/media/oregon pic.4190a326.jpg"
+  },
+  {
+    "revision": "cd925278411e38acdab2038a9a167e59",
+    "url": "/static/media/quiz screen cap.cd925278.png"
+  },
+  {
+    "revision": "120f85dc5b45adfe69b009e79cc828a7",
+    "url": "/static/media/readme screen cap.120f85dc.png"
+  },
+  {
+    "revision": "81b29c745bf6685237f89878fa745a5c",
+    "url": "/static/media/social streaming cap.81b29c74.png"
+  }
+]);
